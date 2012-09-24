b548df4e59fee7ca3c4c315a989b01cf511250b1
zhanghua <airowner@zhanghua.local>
2012-09-25 01:13:20 +0800

    add data.php to prepare store taobao data

---
+++ data.php
@@ -0,0 +1,267 @@
+<?php
+$item = array (
+    'approve_status' => 'onsale',
+    'auction_point' => 0,
+    'cid' => 50000436,
+    'delist_time' => '2012-09-26 16:31:08',
+    'desc' => '<a name="superboss_haibao_superboss_start_JRjaQf"></a><p><a href="http://infay.taobao.com" target="_blank"><img style="margin: 0.0px;width: 750.0px;height: 322.0px;float: none;" src="http://img04.taobaocdn.com/imgextra/i4/170522/T2yy5PXcdXXXXXXXXX_!!170522.jpg"></a></p><a name="superboss_haibao_superboss_end_JRjaQf"></a><br style="display: none;"><p><img src="http://img02.taobaocdn.com/imgextra/i2/170522/T2N2iDXfNaXXXXXXXX_!!170522.jpg"><br><img src="http://img03.taobaocdn.com/imgextra/i3/170522/T28vqDXcXaXXXXXXXX_!!170522.jpg"><br><img src="http://img04.taobaocdn.com/imgextra/i4/170522/T2TfuDXaRaXXXXXXXX_!!170522.jpg"><br><img src="http://img03.taobaocdn.com/imgextra/i3/170522/T2NLSDXhtXXXXXXXXX_!!170522.jpg"><br><img src="http://img02.taobaocdn.com/imgextra/i2/170522/T2XfWDXg4XXXXXXXXX_!!170522.jpg"><br><img src="http://img03.taobaocdn.com/imgextra/i3/170522/T2ze5DXntaXXXXXXXX_!!170522.jpg"><br><img src="http://img04.taobaocdn.com/imgextra/i4/170522/T24K1DXopaXXXXXXXX_!!170522.jpg"><br><img src="http://img04.taobaocdn.com/imgextra/i4/170522/T2h2KDXllXXXXXXXXX_!!170522.jpg"><br><img src="http://img02.taobaocdn.com/imgextra/i2/170522/T28vSDXhXXXXXXXXXX_!!170522.jpg"><br><img src="http://img03.taobaocdn.com/imgextra/i3/170522/T20u9DXk0aXXXXXXXX_!!170522.jpg"><br><img src="http://img02.taobaocdn.com/imgextra/i2/170522/T2N2aDXjBaXXXXXXXX_!!170522.jpg"><br><img src="http://img04.taobaocdn.com/imgextra/i4/170522/T2dvyDXalaXXXXXXXX_!!170522.jpg"><br><img src="http://img04.taobaocdn.com/imgextra/i4/170522/T2P2yDXo0XXXXXXXXX_!!170522.jpg"></p>',
+    'detail_url' => 'http://item.taobao.com/item.htm?id=8729856082',
+    'ems_fee' => '15.00',
+    'express_fee' => '0.00',
+    'freight_payer' => 'buyer',
+    'has_discount' => false,
+    'has_invoice' => false,
+    'has_showcase' => true,
+    'has_warranty' => false,
+    'input_pids' => '1632501',
+    'input_str' => 'Y21310329',
+    'is_virtual' => false,
+    'item_imgs' => 
+    array (
+        'item_img' => 
+        array (
+            0 => 
+            array (
+                'id' => 0,
+                'position' => 0,
+                'url' => 'http://img02.taobaocdn.com/bao/uploaded/i2/T1ysK2XlpjXXaL1WQ5_055000.jpg',
+            ),
+            1 => 
+            array (
+                'id' => 884307010,
+                'position' => 1,
+                'url' => 'http://img04.taobaocdn.com/bao/uploaded/i4/170522/T2M3tKXkxbXXXXXXXX_!!170522.jpg',
+            ),
+            2 => 
+            array (
+                'id' => 885214738,
+                'position' => 2,
+                'url' => 'http://img01.taobaocdn.com/bao/uploaded/i1/170522/T2Dx0wXj4XXXXXXXXX_!!170522.jpg',
+            ),
+            3 => 
+            array (
+                'id' => 884308706,
+                'position' => 3,
+                'url' => 'http://img02.taobaocdn.com/bao/uploaded/i2/170522/T2AOXwXb8XXXXXXXXX_!!170522.jpg',
+            ),
+            4 => 
+            array (
+                'id' => 884309170,
+                'position' => 4,
+                'url' => 'http://img02.taobaocdn.com/bao/uploaded/i2/170522/T2bOxwXfpXXXXXXXXX_!!170522.jpg',
+            ),
+        ),
+    ),
+    'list_time' => '2012-09-19 16:31:08',
+    'location' => 
+    array (
+        'city' => '广州',
+        'state' => '广东',
+    ),
+    'modified' => '2012-09-25 00:32:51',
+    'nick' => '风中点烟',
+    'num' => 38,
+    'num_iid' => 8729856082,
+    'outer_id' => 'Y21310329',
+    'pic_url' => 'http://img02.taobaocdn.com/bao/uploaded/i2/T1ysK2XlpjXXaL1WQ5_055000.jpg',
+    'post_fee' => '0.00',
+    'postage_id' => 42564682,
+    'price' => '620.00',
+    'prop_imgs' => 
+    array (
+        'prop_img' => 
+        array (
+            0 => 
+            array (
+                'id' => 884309490,
+                'position' => 0,
+                'properties' => '1627207:28340',
+                'url' => 'http://img01.taobaocdn.com/bao/uploaded/i1/170522/T2KAtKXkhbXXXXXXXX_!!170522.jpg',
+            ),
+            1 => 
+            array (
+                'id' => 3873347519,
+                'position' => 0,
+                'properties' => '1627207:28324',
+                'url' => 'http://img03.taobaocdn.com/bao/uploaded/i3/170522/T2qR8CXdFNXXXXXXXX_!!170522.jpg',
+            ),
+        ),
+    ),
+    'property_alias' => '1627207:28324:米黄-K2;1627207:28340:宝蓝-N8;20503:3271528:M;20503:25033336:S;20503:3267945:XXL;20503:3271531:XL;20503:3271530:L',
+    'props' => '1627743:3267185;1632501:86201943;1627746:29449;1627207:28324;1627207:28340;20503:25033336;20503:3271528;20503:3271530;20503:3271531;20503:3267945;1627766:10122;1627773:10122;20000:4535964;1627782:112571;14043904:27924515;20606:29452;19166310:44702;19166829:41036269;2098004:5493699;2097794:3267959',
+    'seller_cids' => ',611506290,296807255,615617646,383908567,539854423,534089100,',
+    'skus' => 
+    array (
+        'sku' => 
+        array (
+            0 => 
+            array (
+                'created' => '2010-12-10 19:20:49',
+                'modified' => '2012-03-22 12:53:50',
+                'outer_id' => 'Y21310329N846',
+                'price' => '620.00',
+                'properties' => '1627207:28340;20503:25033336',
+                'quantity' => 0,
+                'sku_id' => 8076074130,
+            ),
+            1 => 
+            array (
+                'created' => '2010-12-10 19:20:49',
+                'modified' => '2012-03-20 13:40:02',
+                'outer_id' => 'Y21310329N854',
+                'price' => '620.00',
+                'properties' => '1627207:28340;20503:3267945',
+                'quantity' => 0,
+                'sku_id' => 8076074146,
+            ),
+            2 => 
+            array (
+                'created' => '2010-12-10 19:20:49',
+                'modified' => '2012-09-22 15:22:10',
+                'outer_id' => 'Y21310329N848',
+                'price' => '620.00',
+                'properties' => '1627207:28340;20503:3271528',
+                'quantity' => 16,
+                'sku_id' => 8076074162,
+            ),
+            3 => 
+            array (
+                'created' => '2010-12-10 19:20:49',
+                'modified' => '2012-09-22 15:22:10',
+                'outer_id' => 'Y21310329N850',
+                'price' => '620.00',
+                'properties' => '1627207:28340;20503:3271530',
+                'quantity' => 1,
+                'sku_id' => 8076074178,
+            ),
+            4 => 
+            array (
+                'created' => '2010-12-10 19:20:49',
+                'modified' => '2012-03-20 13:40:02',
+                'outer_id' => 'Y21310329N852',
+                'price' => '620.00',
+                'properties' => '1627207:28340;20503:3271531',
+                'quantity' => 0,
+                'sku_id' => 8076074194,
+            ),
+            5 => 
+            array (
+                'created' => '2012-04-10 14:14:15',
+                'modified' => '2012-04-26 12:29:05',
+                'outer_id' => 'Y21310329K246',
+                'price' => '620.00',
+                'properties' => '1627207:28324;20503:25033336',
+                'quantity' => 0,
+                'sku_id' => 19142518229,
+            ),
+            6 => 
+            array (
+                'created' => '2012-04-10 14:14:15',
+                'modified' => '2012-09-22 15:22:10',
+                'outer_id' => 'Y21310329K254',
+                'price' => '620.00',
+                'properties' => '1627207:28324;20503:3267945',
+                'quantity' => 1,
+                'sku_id' => 19142518230,
+            ),
+            7 => 
+            array (
+                'created' => '2012-04-10 14:14:15',
+                'modified' => '2012-09-22 15:22:10',
+                'outer_id' => 'Y21310329K248',
+                'price' => '620.00',
+                'properties' => '1627207:28324;20503:3271528',
+                'quantity' => 7,
+                'sku_id' => 19142518231,
+            ),
+            8 => 
+            array (
+                'created' => '2012-04-10 14:14:15',
+                'modified' => '2012-09-22 15:22:10',
+                'outer_id' => 'Y21310329K250',
+                'price' => '620.00',
+                'properties' => '1627207:28324;20503:3271530',
+                'quantity' => 7,
+                'sku_id' => 19142518232,
+            ),
+            9 => 
+            array (
+                'created' => '2012-04-10 14:14:15',
+                'modified' => '2012-09-25 00:32:51',
+                'outer_id' => 'Y21310329K252',
+                'price' => '620.00',
+                'properties' => '1627207:28324;20503:3271531',
+                'quantity' => 6,
+                'sku_id' => 19142518233,
+            ),
+        ),
+    ),
+    'stuff_status' => 'new',
+    'title' => '9.25大放价古老鲨鱼专柜正品 秋冬男装纯棉休闲条纹翻领长袖T恤',
+    'type' => 'fixed',
+    'valid_thru' => 7,
+);
+$shop = array (
+    'taobaoke_shops' => 
+    array (
+        'taobaoke_shop' => 
+        array (
+            0 => 
+            array (
+                'auction_count' => '139',
+                'click_url' => 'http://s.click.taobao.com/t?e=zGU34CA7K%2BPkqB04MQzdgG69RGcaJPb63yl1mhTcw22Q95w1yiSvAtDolqdEfm%2FJai2H%2FW5ZzquVunEhufF3vXtDWxpkcYAXE2B5cG3iKg4nJLE%2BhP8EVGeS6OYch5NKS8pJM%2BVDd0i0VKyV%2F0Wu2GbA&pid=mm_17531361_0_0&spm=2014.21101696.1.0',
+                'commission_rate' => '5.0',
+                'seller_credit' => '5',
+                'shop_title' => '意大利古老鲨鱼淘宝专卖店',
+                'shop_type' => 'C',
+                'total_auction' => '22',
+                'user_id' => 287690350,
+            ),
+            1 => 
+            array (
+                'auction_count' => '184',
+                'click_url' => 'http://s.click.taobao.com/t?e=zGU34CA7K%2BPkqB04MQzdgG69RGcaJPb63yl1mhTcw22Q95w1yiSvAtDolqdEfm%2FJai2DsgsRV8LbfExgx%2BJsYxN8STr4DeADS9a4Q2VTn2go5obslMg%2FplP0P3zEuOxawpRKYZS57B0iQuUGaqDjgEYL&pid=mm_17531361_0_0&spm=2014.21101696.1.0',
+                'commission_rate' => '6.82',
+                'seller_credit' => '8',
+                'shop_title' => '外贸旗舰店-国际品牌香港原单专柜同步boss 古老鲨鱼 长袖t恤',
+                'shop_type' => 'C',
+                'total_auction' => '65',
+                'user_id' => 618141874,
+            ),
+            2 => 
+            array (
+                'auction_count' => '671',
+                'click_url' => 'http://s.click.taobao.com/t?e=zGU34CA7K%2BPkqB04MQzdgG69RGcaJPb63yl1mhTcw22Q95w1yiSvAtDolqdEfm%2FJai2EfT%2F5MEsDvL95lB6bP5UNWNMcSNVdHqOVQC6Zz1qrxq7QM2IGZPYyDWKgcKPiu8HzbQ1CVHV24sbhvkUM&pid=mm_17531361_0_0&spm=2014.21101696.1.0',
+                'commission_rate' => '9.96',
+                'seller_credit' => '14',
+                'shop_title' => '古老鲨鱼直营店',
+                'shop_type' => 'C',
+                'total_auction' => '8469',
+                'user_id' => 170522,
+            ),
+            3 => 
+            array (
+                'auction_count' => '558',
+                'click_url' => 'http://s.click.taobao.com/t?e=zGU34CA7K%2BPkqB04MQzdgG3VSuWRIvnJbEpKV5LvWuPPB5%2FPy35LbkKLRBgJBYvICbcj9Yx40L7JC92%2BkoXERQodRsr8%2F6qa3W8%2BUcSE1lmpoy2JdCxvUyTextVLkM1xTRzzcMBuQQAELgJa9rq1vYZ1&pid=mm_17531361_0_0&spm=2014.21101696.1.0',
+                'commission_rate' => '4.96',
+                'seller_credit' => '11',
+                'shop_title' => '古老鲨鱼旗舰店',
+                'shop_type' => 'B',
+                'total_auction' => '10015',
+                'user_id' => 556921063,
+            ),
+            4 => 
+            array (
+                'auction_count' => '244',
+                'click_url' => 'http://s.click.taobao.com/t?e=zGU34CA7K%2BPkqB04MQzdgG69RGcaJPb63yl1mhTcw22Q95w1yiSvAtDolqdEfm%2FJai2AgEUDH0uYD9L3Zj4iiKfz6P2nmcQ%2BI6MtLpqa3xld47I3L4yonbxyZIidKoVWppgIvhRO%2F9Ft2HmVRTte0Fp6&pid=mm_17531361_0_0&spm=2014.21101696.1.0',
+                'commission_rate' => '5.28',
+                'seller_credit' => '9',
+                'shop_title' => 'LEE 保罗 古老鲨鱼专柜代购店',
+                'shop_type' => 'C',
+                'total_auction' => '263',
+                'user_id' => 597636485,
+            ),
+        ),
+    ),
+    'total_results' => 5,
+);
