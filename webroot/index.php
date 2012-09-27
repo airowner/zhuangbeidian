01d5f4a2fb02671aeb048337acb7b3e02338b29e
zhanghua <airowner@localhost.localdomain>
2012-09-27 09:07:12 +0800

    update

--- webroot/index.php
+++ webroot/index.php
@@ -61,9 +61,10 @@ if (!defined('APP_DIR')) {
  */
 if(PHP_OS == 'WINNT'){
     define('CAKE_CORE_INCLUDE_PATH', 'E:' . DS . 'cakephp' . DS . 'lib');
-}else{
+}elseif(PHP_OS == 'Darwin'){
     define('CAKE_CORE_INCLUDE_PATH', '/Users/airowner/Sites/cakephp' . DS . 'lib');
-    #define('CAKE_CORE_INCLUDE_PATH', '/srv/http/git/cakephp' . DS . 'lib');
+}else{
+    define('CAKE_CORE_INCLUDE_PATH', '/srv/http/git/cakephp' . DS . 'lib');
 }
 
 /**
